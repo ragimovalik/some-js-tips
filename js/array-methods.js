c42529883fc8a493f0b19a9a0e5f856daea4d8fb
ragimovalik <ragimovalik@gmail.com>
2021-06-18 20:25:31 +0300

    Reduce function added

--- js/array-methods.js
+++ js/array-methods.js
@@ -1,17 +1,19 @@
-const numbers = [1, 11, 5, 8, 65, 38, 14];
+const numbers = [1, 11, 5, 8, 38, 14];
 
 // ========== reduce example ==========
 
-const reduce = (arr, callback, initialValue) => {
-  let result = initialValue;
+const reduce = (arr, callback /*, initialValue*/) => {
+  let result = arr[0]; /* || initialValue */
 
-  for (const number of arr) {
-    result = sum(number, result);
+  for (let i = 1; i < arr.length; i++) {
+    result = callback(arr[i], result);
   }
 
   return result;
 };
 
 const sum = (a, b) => a + b;
+const multpl = (a, b) => a * b;
 
-console.log(reduce(numbers, sum, 0));
+console.log(reduce(numbers, sum /*, 0 */));
+console.log(reduce(numbers, multpl));
